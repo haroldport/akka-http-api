22ab0420a3cbc993841c4a03f895d7a6c75a4038
Harold Portocarrero <hportocarrero@todo1.com>
2018-10-26 11:48:55 -0500

    Http Server ready!!

--- src/test/com/example/scala_http_api/ScalaHttpApiTest.scala
+++ src/test/com/example/scala_http_api/ScalaHttpApiTest.scala
@@ -1,23 +1,15 @@
 package com.example.scala_http_api
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
-import akka.http.scaladsl.server.Directives.{complete, get, path}
+import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.concurrent.ScalaFutures
 
 final class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
 
-  private val routesWithDefinedResponses =
-    get {
-      path("status") {
-        complete(HttpEntity(ContentTypes.`application/json`, """{"status": "ok"}"""))
-      }
-    }
-
   "ScalaHttpApi" should {
     "respond successfully while requesting its status" in {
-      Get("/status") ~> routesWithDefinedResponses ~> check {
+      Get("/status") ~> Routes.all ~> check {
         status shouldBe StatusCodes.OK
         contentType shouldBe ContentTypes.`application/json`
         entityAs[String] shouldBe """{"status": "ok"}"""
